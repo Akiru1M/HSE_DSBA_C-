b4fc3dc04d5528a5fe1fe251c78c4a56fe64670a
Akiru1M <m1akiru1m@gmail.com>
2025-09-08 18:12:15 +0300

    second

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
+using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-
-    std::cout << "Hello world";
-
+    return 0;
 }
