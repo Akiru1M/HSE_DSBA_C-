89383fc0d5439e2495e27579f51cd3c6fa3d055d
Akiru1M <m1akiru1m@gmail.com>
2025-09-15 20:57:37 +0300

    clear project

--- main.cpp
+++ main.cpp
@@ -4,22 +4,6 @@
 
 int main() {
 
-    int a[5] = {1,2,3,4,5};
-    int* start = a;
-    int* end = start + 4;
-
-    while (start < end) {
-        int temp = *start;
-        *start = *end;
-        *end = temp;
-        ++start;
-        --end;
-    }
-
-
-    std::cout << a[4] << " " << a[0] << std::endl;
-
-
 }
 
 
