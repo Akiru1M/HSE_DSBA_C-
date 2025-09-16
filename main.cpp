0d434d329ee874f06118290c13ddd7fe06ef583e
Akiru1M <m1akiru1m@gmail.com>
2025-09-16 10:05:04 +0300

    contest_3_task_1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
 int main () {
-    std::cout << "Hello World!\n";
+    int x1, y1;
+    int x2, y2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    if (x2-x1 == y2-y1) {
+        std::cout << "YES";
+    } else if (y1 == y2) {
+        std::cout << "YES";
+    } else if (x1 == x2) {
+        std::cout << "YES";
+    } else {
+        std::cout << "NO";
+    }
+
 }
