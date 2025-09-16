c903aede93385f1db0adc4f12e96116f321027d3
Akiru1M <m1akiru1m@gmail.com>
2025-09-16 10:19:12 +0300

    contest_3_task_2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
 int main () {
-    int x1, y1;
-    int x2, y2;
-    std::cin >> x1 >> y1 >> x2 >> y2;
-
-    if (x2-x1 == y2-y1) {
-        std::cout << "YES";
-    } else if (y1 == y2) {
-        std::cout << "YES";
-    } else if (x1 == x2) {
-        std::cout << "YES";
-    } else {
-        std::cout << "NO";
+    int n;
+    std::cin >> n;
+    int ans;
+    while (n>0) {
+        ans += n%10;
+        n /= 10;
     }
-
+    std::cout << ans;
 }
