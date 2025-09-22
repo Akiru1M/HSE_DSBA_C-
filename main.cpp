6fbabe2ff07541ba4fa427ca01ef79fa621efc47
Akiru1M <m1akiru1m@gmail.com>
2025-09-22 19:43:14 +0300

    Word_reveal_animation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
+int main() {
 
-int main () {
-    int n;
-    std::cin >> n;
-    int ans;
-    while (n>0) {
-        ans += n%10;
-        n /= 10;
+
+    int count = 0;
+
+    std::string words[3] {"I Love DSBA.", "I Love HSE.", "I Love Seminars on C++"};
+
+    while (true) {
+        for (std::string word : words) {
+            count=0;
+            for (char i: word) {
+                std::cout << "\u001b[31m" << i;
+                count++;
+                std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                }
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            for (int i = 0; i < count; i++) {
+                std::cout << '\b' << ' ' << '\b';
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }
     }
-    std::cout << ans;
 }
